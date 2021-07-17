3fb38fb3f0c9eebbbb4ccdcc214e0de1d8c4d4e6
ayberkbozkus <32310900+ayberkbozkus@users.noreply.github.com>
2021-07-17 15:06:02 +0300

    Course Class add admin page

--- django_project/smartedu_con/courses/admin.py
+++ django_project/smartedu_con/courses/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from . models import Course
 
-admin.site.register(Course)
 
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'available')
+    list_filter = ('available',)
+    search_fields = ('name', 'description')
