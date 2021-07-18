7acd77c567704c86d0df40f6af71c4241c2a1c10
ayberkbozkus <32310900+ayberkbozkus@users.noreply.github.com>
2021-07-18 10:01:00 +0300

    courses for loop

--- django_project/smartedu_con/courses/models.py
+++ django_project/smartedu_con/courses/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Course(models.Model):
     name = models.CharField(max_length=200, unique=True)
     description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="courses/default_course_image.jpg")
+    image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="courses/default_course_image.jpeg")
     date = models.DateTimeField(auto_now=True)
     available = models.BooleanField(default=True)
 
