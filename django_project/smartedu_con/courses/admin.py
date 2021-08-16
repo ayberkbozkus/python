92dc13c159b5edaae6e7050479ccd9cc8c326aee
ayberkbozkus <32310900+ayberkbozkus@users.noreply.github.com>
2021-08-16 12:14:08 +0300

    category part

--- django_project/smartedu_con/courses/admin.py
+++ django_project/smartedu_con/courses/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Course
+from . models import Course, Category
 
 
 @admin.register(Course)
@@ -7,3 +7,7 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ('name', 'available')
     list_filter = ('available',)
     search_fields = ('name', 'description')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('name',)}
