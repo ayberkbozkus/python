efc17db8a57deeb325ca6cac86adff27708a8794
ayberkbozkus <32310900+ayberkbozkus@users.noreply.github.com>
2021-08-16 22:52:39 +0300

    n11 product bot

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -2,13 +2,26 @@ from bs4 import BeautifulSoup
 import requests
 from csv import writer
 
-base_url = 'http://quotes.toscrape.com/'
+base_url = 'https://www.n11.com/telefon-ve-aksesuarlari/cep-telefonu'
 response = requests.get(base_url)
 
-soup1 = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response.text, 'html.parser')
 
-for quote in soup1.select('.quote'):
-    text = quote.find(class_='text').get_text()
-    author = quote.find(class_='author').get_text()
-    link = base_url + quote.find('a')['href']
-    print(author, text, link,'\n-------------------------------')
+products = soup.find_all("li",attrs={"class":"column"})
+
+for product in products:
+    productName = product.a.get("title")
+    productLink = product.a.get("href")
+    
+    try: 
+        product_r = requests.get(productLink)
+    except:
+        print("!!! Not Found !!!")
+
+    product_soup = BeautifulSoup(product_r.text)
+    features = product_soup.find_all("div", attrs={"class": "unf-prop"})
+    for feature in features:
+        print(productName)
+        print(feature.find("p",attrs={"class":"unf-prop-list-title"}).text)
+        print(feature.find("p",attrs={"class":"unf-prop-list-prop"}).text)
+    print("#"*10)
